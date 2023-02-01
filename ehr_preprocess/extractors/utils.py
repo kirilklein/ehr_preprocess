0e172b7f0fa62f7bd0837aedd0d2d5e6ad82f146
Kiril Klein <kikl@di.ku.dk>
2023-02-01 13:24:30 +0100

    renamed

--- ehr_preprocess/extractors/utils.py
+++ ehr_preprocess/extractors/utils.py
@@ -1,7 +1,39 @@
 import pandas as pd
-import os
+import os, sys
 from os.path import join, split
 from tqdm import tqdm
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+    
 
 class MIMIC_CSV_to_Parquet_Converter:
     """Converts all csv files in a directory to parquet files"""
@@ -95,5 +127,5 @@ def pandas_get_columns(path):
 
 drop_columns_dic = {
     'CHARTEVENTS.csv.gz': ['RESULTSTATUS', 'STOPPED','WARNING', 'STORETIME', 'CGID', 'VALUENUM', 'ROW_ID'],
-    'DATETIMEEVENTS.csv.gz': ['ROW_ID', 'VALUEUOM', 'WARNING', 'RESULTSTATUS', 'STOPPED',
+    'DATETIMEEVENTS.csv.gz': ['ROW_ID', 'VALUEUOM', 'WARNING', 'RESULTSTATUS', 'STOPPED',]
 }
