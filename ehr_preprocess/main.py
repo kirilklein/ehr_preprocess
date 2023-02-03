75a61087eecafe91a8d8b2248728fed1e069514a
Kiril Klein <kikl@di.ku.dk>
2023-02-03 11:21:36 +0100

    changes to notebook

--- ehr_preprocess/main.py
+++ ehr_preprocess/main.py
@@ -3,7 +3,7 @@ from os.path import dirname, join, realpath
 import hydra
 from omegaconf import DictConfig
 
-from ehr_preprocess.preprocessors import preprocessors, utils
+from ehr_preprocess.processors import utils, processors
 
 config_name = "config"
 base_dir = dirname(dirname(realpath(__file__)))
@@ -11,10 +11,10 @@ config_path = join(base_dir, 'configs')
 
 @hydra.main(version_base=None, config_path=config_path, config_name=config_name)
 def my_app(cfg: DictConfig) -> None:
-    print('config:', cfg)
-    if cfg.convert_csv_to_parquet:
-        csv_to_parquet = utils.MIMIC_CSV_to_Parquet_Converter(cfg, test=False)
-        csv_to_parquet()
+    processor = hydra.utils.instantiate(cfg.processor, cfg=cfg)
+    #if cfg.convert_csv_to_parquet:
+     #   csv_to_parquet = utils.MIMIC_CSV_to_Parquet_Converter(cfg, test=False)
+      #  csv_to_parquet()
     # hydra.utils.instantiate(cfg)
     
 
