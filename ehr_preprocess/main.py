7b8b9a02ce62eac5f56ac799c5badcbf8264560e
Kiril Klein <kikl@di.ku.dk>
2023-02-04 15:40:22 +0100

    lab tests preprocessing finished

--- ehr_preprocess/main.py
+++ ehr_preprocess/main.py
@@ -3,7 +3,7 @@ from os.path import dirname, join, realpath
 import hydra
 from omegaconf import DictConfig
 
-from ehr_preprocess.processors import utils, processors
+from ehr_preprocess/preprocessors import utils, preprocessors
 
 config_name = "config"
 base_dir = dirname(dirname(realpath(__file__)))
@@ -11,7 +11,7 @@ config_path = join(base_dir, 'configs')
 
 @hydra.main(version_base=None, config_path=config_path, config_name=config_name)
 def my_app(cfg: DictConfig) -> None:
-    processor = hydra.utils.instantiate(cfg.processor, cfg=cfg)
+    preprocessor = hydra.utils.instantiate(cfg/preprocessor, cfg=cfg)
     #if cfg.convert_csv_to_parquet:
      #   csv_to_parquet = utils.MIMIC_CSV_to_Parquet_Converter(cfg, test=False)
       #  csv_to_parquet()
--- ehr_preprocess/preprocessors/mimic.py
+++ ehr_preprocess/preprocessors/mimic.py
@@ -1,12 +1,14 @@
 from os.path import dirname, join, split, realpath
 import os
-from ehr_preprocess.processors import utils
+import json
+from ehr_preprocess.preprocessors import utils
 import pandas as pd
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
+
 base_dir = dirname(dirname(dirname(realpath(__file__))))
         
 
-class BaseProcessor():
+class BasePreprocessor():
     def __init__(self, cfg, test=False) -> None:
         self.cfg = cfg
         self.raw_data_path = cfg.paths.raw_data_path
@@ -38,43 +40,40 @@ class BaseProcessor():
             'VALUENUM': 'valuenum', 
             'VALUEUOM': 'unit'
         }
-        self.metadata_dic = {'Type':[], 'System':[]}
+        self.metadata_ls = []
+    def save_metadata(self):
+        with open(join(self.processed_data_path, 'metadata.json'), 'w') as fp:
+            json.dump(self.metadata_ls, fp)
 
-class MIMIC3Processor(BaseProcessor):
+class MIMIC3Preprocessor(BasePreprocessor):
     """Extracts events from MIMIC-III database and saves them in a single file."""
     def __init__(self, cfg, test=False):
-        super(MIMIC3Processor, self).__init__(cfg, test)
+        super(MIMIC3Preprocessor, self).__init__(cfg, test)
 
-    def forward(self):
+    def __call__(self):
+        print('Preprocess Mimic3 from: ', self.raw_data_path)
+        print(":Extract patient info")
         self.extract_patient_info()
         print(":Extract events")
-        print("::Extract diagnoses")
-        print("::Extract prescriptions")
-        print("::Extract lab results")
-        print("::Extract vital signs")
-        print("::Extract admissions")
-        print('Preprocess Mimic3 from: ', self.raw_data_path)
+        self.extract_events()
         self.save_metadata()
-    def extract_events():
+
+    def extract_events(self):
+        self.extract_lab()
         pass
 
     def extract_patient_info():
-        print(":Extract patient info")
+        pass
         
     def extract_lab(self):
-        df = pd.read_csv(join(self.raw_data_path, 'LABEVENTS.csv'), 
-            nrows=self.nrows, usecols=['SUBJECT_ID', 'ITEMID', 'CHARTTIME', 'VALUE', 'VALUENUM','VALUEUOM'],
-            parse_dates=['CHARTTIME'], dtype={'SUBJECT_ID': 'Int32', 'ITEMID': 'Int32', 'VALUE': 'str', 'VALUENUM': 'float32', 'VALUEUOM': 'str'},
-            compression='gzip')
-        df = df.rename(columns=self.rename_dic)
-        self.metadata_dic['Type'].append('Lab')
-        self.metadata_dic['System'].append('LOINC')
+        print("::Extract lab results")
+        MIMICLabPreprocessor(self.cfg, self.test)()
+        self.metadata_ls.append({
+            'Type':'Lab', 'System':'LOINC', 'File':'lab.parquet', 'Prepend':'L'
+        })
 
     def extract_diagnoses():
         pass
-    
-    def save_metadata(self):
-        pd.DataFrame(self.metadata_dic).to_parquet(join(self.processed_data_path, 'metadata.parquet'))
         
     def extract_prescriptions():
         pass
@@ -82,9 +81,74 @@ class MIMIC3Processor(BaseProcessor):
     def extract_procedures():
         pass
 
+class MIMICLabPreprocessor(MIMIC3Preprocessor):
+
+    def __init__(self, cfg, test=False):
+        super(MIMICLabPreprocessor, self).__init__(cfg, test)
+    
+    def __call__(self):
+        df = self.load()
+        df_dic = self.load_dic()
+        df = self.preprocess(df, df_dic)
+        df.to_parquet(join(self.processed_data_path, 'lab.parquet'))
+
+    def load(self):
+        df = pd.read_csv(join(self.raw_data_path, 'LABEVENTS.csv.gz'), 
+            compression='gzip', nrows=self.nrows, parse_dates=['CHARTTIME'], 
+            dtype={'SUBJECT_ID': 'Int32', 'ITEMID': 'Int32', 'VALUE': 'str', 'VALUENUM': 'float32', 'VALUEUOM': 'str', 'HADM_ID': 'Int32'})
+        df = df.rename(columns={'SUBJECT_ID': 'PID', 'CHARTTIME': 'TIMESTAMP', 'VALUEUOM': 'VALUE_UNIT', 'HADM_ID': 'ADMISSION_ID'}).drop(columns=['ROW_ID', 'FLAG'])
+        return df
+    
+    def load_dic(self):
+        return pd.read_csv(join(self.raw_data_path, 'D_LABITEMS.csv.gz'), compression='gzip')
+
+    def preprocess_dic(self, df_dic):
+        # add more specification to SPECIMEN using the fluid column
+        df_dic.loc[df_dic['LABEL'].str.contains('SPECIMEN'), 'LABEL'] \
+            = df_dic.loc[df_dic['LABEL'].str.contains('SPECIMEN'), 'FLUID']\
+                + ' ' + df_dic.loc[df_dic['LABEL'].str.contains('SPECIMEN'), 'CATEGORY']
+        return df_dic
+    
+    def preprocess(self, df, df_dic):
+        concept_map = self.get_concept_map(df_dic)
+        df['CONCEPT'] = df.ITEMID.map(concept_map)
+        df.drop(columns=['ITEMID'], inplace=True)
+        df = self.process_values(df)
+        pass
+        
+    def get_concept_map(self, df_dic):
+        item_code_dic = pd.Series(df_dic.LOINC_CODE.values, index=df_dic.ITEMID).to_dict()
+        item_name_dic = pd.Series(df_dic[df_dic.LOINC_CODE.isna()].LABEL.values, index=df_dic[df_dic.LOINC_CODE.isna()].ITEMID).to_dict()
+        # combine dicts
+        return {**item_code_dic, **item_name_dic}
+
+    def process_values(self, df):
+        df_cont = self.process_continuous_values(df)
+        df_cat = self.process_categorical_values(df)
+        df = pd.concat([df_cont, df_cat])
+        # sort by PID and ADMISSION_ID
+        df.sort_values(by=['PID', 'ADMISSION_ID', 'TIMESTAMP'], inplace=True)
+        df.CONCEPT = df.CONCEPT.map(lambda x: 'L'+x)
+        df.to_parquet(join(self.processed_data_path, 'concept.lab.parquet'))
+
+    def process_continuous_values(df):
+        df_cont = df[df['VALUENUM'].notnull()]
+        df_cont = df_cont[df_cont['VALUENUM'] >= 0]
+        df_cont.drop(columns=['VALUE'], inplace=True)
+        df_cont.rename(columns={'VALUENUM': 'VALUE'}, inplace=True)
+        df_cont.drop(columns=['VALUE_NUM'], inplace=True)
+        df_cont['VALUE_CAT'] = 'NaN'
+        return df_cont
+
+    def process_categorical_values(df):
+        df_cat = df[df['VALUENUM'].isnull()]
+        df_cat['VALUE_CAT'] = df_cat['VALUE']
+        df_cat['VALUE'] = df_cat.groupby('ITEMID')['VALUE'].transform(lambda x: x.astype('category').cat.codes)
+        df_cat.drop(columns=['VALUE_NUM'], inplace=True)
+        df_cat['VALUE_UNIT'] = 'categorical'
+        return df_cat
     
 if __name__ == '__main__':
-    cfg = {'data': {'raw_data_dir': 'C:\\Users\\user\\Documents\\GitHub\\ehr_preprocess\\data\\raw\\mimic-iii-clinical-database-1.4'}}
-    omegacfg = DictConfig(cfg)
-    processor = MIMIC3Processor(omegacfg)
+    cfg = OmegaConf.load(join(base_dir, "configs", "mimic3.yaml"))
+    preprocessor = MIMIC3Preprocessor(cfg, test=True)
     
