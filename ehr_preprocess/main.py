84e39ce9bd76a8c39531d79bf7fa783ae6f04627
Kiril Klein <kikl@di.ku.dk>
2023-05-28 13:31:50 +0200

    No hydra on azure

--- ehr_preprocess/main.py
+++ ehr_preprocess/main.py
@@ -1,19 +1,17 @@
 from os.path import dirname, join, realpath
 
-import hydra
-from hydra.utils import instantiate
-from omegaconf import DictConfig
+from preprocessors.load import instantiate, load_config
+import yaml
 
-config_name = "synthea"
-base_dir = dirname(dirname(realpath(__file__)))
-config_path = join(base_dir, 'configs')
-
-@hydra.main(version_base=None, config_path=config_path, config_name=config_name)
-def my_app(cfg: DictConfig) -> None:
-    preprocessor = instantiate(cfg.preprocessor, cfg=cfg)
+config_name = "azure"
+def my_app(config_name):
+    base_dir = dirname(dirname(realpath(__file__)))
+    config_path = join(base_dir, 'configs')
+    cfg = load_config(join(config_path, config_name+'.yaml'))
+    preprocessor = instantiate(cfg.preprocessor, {'cfg':cfg})
     preprocessor()
     
     
 
 if __name__=='__main__':
-    my_app()
+    my_app(config_name)
