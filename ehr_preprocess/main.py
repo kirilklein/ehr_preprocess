b8ba3c49262c1bd637c0a82726085f78c455c2ce
Kiril Klein <kikl@di.ku.dk>
2023-07-30 12:16:32 +0200

    update main

--- ehr_preprocess/main.py
+++ ehr_preprocess/main.py
@@ -1,17 +1,30 @@
 from os.path import dirname, join, realpath
-
+import shutil
 from preprocessors.load import instantiate, load_config
-import yaml
+import logging
+from azure_run.run import Run
+from azure_run import datastore
+
+
+run = Run
+run.name(f"Diagnoses_Medication_Debugged")
+ds_sp = datastore("sp_data")
 
 config_name = "azure"
 def my_app(config_name):
-    base_dir = dirname(dirname(realpath(__file__)))
+    # datastore = Datastore.get(ws, 'workspaceblobstore')
+    base_dir = dirname(realpath(__file__))
     config_path = join(base_dir, 'configs')
     cfg = load_config(join(config_path, config_name+'.yaml'))
-    preprocessor = instantiate(cfg.preprocessor, {'cfg':cfg})
+    shutil.copyfile(join(config_path, config_name+'.yaml'), join(cfg.paths.output_dir, 'config.yaml'))
+    logging.basicConfig(filename=join(cfg.paths.output_dir, cfg.run_name+'.log'), level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    logger = logging.getLogger(__name__)
+    preprocessor = instantiate(cfg.preprocessor, {'cfg':cfg, 'logger':logger, 'datastore':ds_sp})
     preprocessor()
     
-    
+        
 
 if __name__=='__main__':
     my_app(config_name)
