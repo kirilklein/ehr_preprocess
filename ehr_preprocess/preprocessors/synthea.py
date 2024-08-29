cd6a533f2a16853f11c37b60845dc951d2ad2c1d
Kiril Klein <kikl@di.ku.dk>
2024-08-29 14:18:30 +0200

    Fix mapping of diagnosis code
    * do type conversion after mapping
    * concepts are mapped on icd10 if code presen, otherwise keep snomed.

--- ehr_preprocess/preprocessors/synthea.py
+++ ehr_preprocess/preprocessors/synthea.py
@@ -9,27 +9,36 @@ class SyntheaPrepocessor(BasePreprocessor):
             individual_dfs = [self.load_csv(cfg) for cfg in top_level_config.values()]
             combined = pd.concat(individual_dfs)
             combined = combined.drop_duplicates(subset=['PID', 'CONCEPT', 'TIMESTAMP'])
-            combined = combined.sort_values('TIMESTAMP')
-            combined.CONCEPT = combined.CONCEPT.astype(str)
+            combined = combined.sort_values(['PID','TIMESTAMP'])
             if type=='diagnose':
                 combined = self.map_snomed_to_icd10(combined)
+            combined.CONCEPT = combined.CONCEPT.astype(str)
             combined = self.clean_concepts(combined, type)
             self.save(combined, f'concept.{type}')
 
     @staticmethod        
     def map_snomed_to_icd10(combined):
         """Map snomed codes to icd10 codes using the provided map"""
+        snomed_to_icd10 = SyntheaPrepocessor.get_snomed_to_icd10_mapping()
+        combined["new_concept"] = combined.CONCEPT.map(snomed_to_icd10)    
+        combined["CONCEPT"] = combined.new_concept.fillna(combined.CONCEPT)
+        combined.drop(columns=["new_concept"], inplace=True)
+        return combined
+    
+    @staticmethod
+    def get_snomed_to_icd10_mapping():
+        """Get the mapping from snomed to icd10 codes"""
         map_df = pd.read_csv("..\\data\\helper\\tls_Icd10cmHumanReadableMap_US1000124_20230301.tsv", sep="\t", )
         map_df = map_df.drop_duplicates(subset="referencedComponentId", keep="first")
+        map_df.dropna(subset=["mapTarget"], inplace=True)
         snomed_to_icd10 = map_df.set_index("referencedComponentId").mapTarget.to_dict() 
-        combined.CONCEPT = combined.CONCEPT.map(snomed_to_icd10)
-        return combined
+        return snomed_to_icd10
     
     @staticmethod
     def clean_concepts(combined, type):
         """Clean up the concepts"""
         combined.CONCEPT = combined.CONCEPT.str.rstrip("?")
-        combined.CONCEPT = combined.CONCEPT.str.replace(".", "")
+        combined.CONCEPT = combined.CONCEPT.str.replace(".", "", regex=False)
         # add D to the beginning of CONCEPT 
         combined.CONCEPT = PREPENDS[type] + combined.CONCEPT
         combined = combined.dropna(subset=["CONCEPT"])
