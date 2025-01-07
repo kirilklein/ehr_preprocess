0e9266058de7585030e3f07749d1d4335fadd169
Maria Elkjær Montgomery <mariaelkjaermontgomery@oscar.local>
2025-01-07 16:03:01 +0100

    circular import fix

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from preprocessors.load import instantiate, load_config
 import logging
 from azure_run.run import Run
 from azure_run import datastore
-import pathlib
+from azureml.core import Dataset
 
+import pathlib
 
 run = Run
 run.name(f"Diagnosis_Medication_Labtest_Procedure")
 ds_sp = datastore("researcher_data")
+dump_path = join("data-backup", "SP-dumps", "2024-06-24")
 
 config_name = "azure"
 def my_app(config_name):
     base_dir = dirname(realpath(__file__))
     config_path = join(base_dir, 'configs')
     cfg = load_config(join(config_path, config_name+'.yaml'))
-    pathlib.Path(cfg.paths.output_dir).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(cfg.paths.output_dir).mkdir(parents=True, exist_ok=True) # added line
     shutil.copyfile(join(config_path, config_name+'.yaml'), join(cfg.paths.output_dir, 'config.yaml'))
     logging.basicConfig(filename=join(cfg.paths.output_dir, cfg.run_name+'.log'), level=logging.INFO, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
