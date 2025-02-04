b9eadb69e548898d734391f49734e5c229d28f8e
Montgomeryyyy <maria.elk.mont@gmail.com>
2025-02-04 11:04:46 +0100

    removed subscription id

--- azure_run/__init__.py
+++ azure_run/__init__.py
@@ -19,7 +19,7 @@ _DATASTORES = {
     "workspaceartifactstore"
 }
 _WS_CONFIG = {
-    "subscription_id": "f8c5aac3-29fc-4387-858a-1f61722fb57a",
+    "subscription_id": "",
     "resource_group": "forskerpl-n0ybkr-rg",
     "workspace_name": "forskerpl-n0ybkr-mlw"
 }
