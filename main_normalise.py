55f882a2f7cbeeeb23a70d48b00d04c2deca7a15
Montgomeryyyy <maria.elk.mont@gmail.com>
2025-02-09 22:13:09 +0100

    add for test

---
+++ main_normalise.py
@@ -0,0 +1,37 @@
+from os.path import dirname, join, realpath
+import shutil
+from preprocessors.load import instantiate, load_config
+import logging
+from azure_run.run import Run
+from azure_run import datastore
+
+import pathlib
+
+run = Run
+run.name(f"norm_test")
+ds = datastore("workspaceblobstore")
+
+config_name = "normalise_labtests"
+def my_app(config_name):
+    # datastore = Datastore.get(ws, 'workspaceblobstore')
+    base_dir = dirname(realpath(__file__))
+    config_path = join(base_dir, 'configs')
+    cfg = load_config(join(config_path, config_name+'.yaml'))
+    pathlib.Path(cfg.paths.output_dir).mkdir(parents=True, exist_ok=True) # added line
+    shutil.copyfile(join(config_path, config_name+'.yaml'), join(cfg.paths.output_dir, 'config.yaml'))
+    logging.basicConfig(filename=join(cfg.paths.output_dir, cfg.run_name+'.log'), level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    logger = logging.getLogger(__name__)
+    preprocessor = instantiate(cfg.preprocessor, {'cfg':cfg, 'logger':logger, 'datastore':ds})
+    preprocessor()
+    
+    if cfg.env=='azure':
+        from azure_run import file_dataset_save
+        file_dataset_save(local_path=join(cfg.paths.output_dir), datastore_name = "workspaceblobstore",
+                    remote_path = join("DEEP_FETAL", "all_formatted_data_csv", cfg.save_name))
+        mount_context.stop()
+        logger.info('Finished')
+
+if __name__=='__main__':
+    my_app(config_name)
